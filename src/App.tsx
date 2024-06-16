4c0362246af23faf58da6f3871c11027d0a6fcef
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-16 17:54:26 +0530

    product component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Welcome from "./pages/Welcome";
 import LoginReg from "./pages/LoginReg";
 import Productview from "./pages/Productview";
 import Whishlist from "./pages/Whishlist";
+import Cart from "./pages/Cart";
+import Shop from "./pages/Shop";
 function App() {
 
   return (
     <>
-    <Whishlist />
+    <Shop />
     </>
   )
 }
---
+++ src/componment/Product.tsx
@@ -0,0 +1,41 @@
+
+export default function Product() {
+    return (
+
+            <div className="col-lg-4 col-sm-6">
+
+                <div className="single-product-item text-center">
+                    <figure className="product-thumb">
+                        <a href="single-product.html"><img src="src/assets/img/new-product-1.jpg" alt="Products" className="img-fluid" /></a>
+                    </figure>
+
+                    <div className="product-details">
+                        <h2><a href="single-product.html">Crown Summit Backpack</a></h2>
+                        <div className="rating">
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star-half"></i>
+                            <i className="fa fa-star-o"></i>
+                        </div>
+                        <span className="price">$52.00</span>
+                        
+                        <a href="single-product.html" className="btn btn-add-to-cart">+ Add to Cart</a>
+                        <a href="single-product.html" className="btn btn-add-to-cart btn-whislist">+
+                            Wishlist</a>
+                        <a href="single-product.html" className="btn btn-add-to-cart btn-compare">+
+                            Compare</a>
+                    </div>
+
+                    <div className="product-meta">
+
+                        <a href="#" data-toggle="tooltip" data-placement="left" title="" data-original-title="Add to Wishlist"><i className="fa fa-heart-o"></i></a>
+
+                    </div>
+                    <span className="product-bedge">New</span>
+                </div>
+
+            </div>
+
+    )
+}
