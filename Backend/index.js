c9e79c7247ba4126f2c9c94a8cfb0c6442b6ef68
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-23 11:04:08 +0530

    add cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,15 @@
 
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
