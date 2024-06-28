e79a05ec83c8eb3351070e172196cb485524cb00
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-28 16:45:25 +0530

    user registation

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -1,10 +1,34 @@
-const db = require('../config/firebaseConfig');
+const db = require("../config/firebaseConfig");
+const bcrypt = require('bcrypt');
+
 
 exports.create = async (req, res) => {
-    try {
-        const docRef = await db.collection('users').add(req.body);
-        res.status(200).send(`Document written with ID: ${docRef.id}`);
-      } catch (error) {
-        res.status(500).send(`Error adding document: ${error}`);
-      }
+  try {
+    const docRef = await db.collection("users").add(req.body);
+    res.status(200).send(`Document written with ID: ${docRef.id}`);
+  } catch (error) {
+    res.status(500).send(`Error adding document: ${error}`);
+  }
 };
+
+exports.registation = async (req, res) => {
+  try {
+    const { name, email, password, repeatpwd } = req.body;
+    // Check if passwords match
+    if (password !== repeatpwd) {
+      return res.status(400).send("Passwords do not match");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+    const docRef = await db.collection("users").add(user);
+    res.status(200).send(`Document written with ID: ${docRef.id}`);
+  } catch (error) {
+    res.status(500).send(`Error adding document: ${error}`);
+  }
+};
+
+
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/usercontroller');
 const router = express.Router();
 
 router.post('/create', userController.create);
+router.post('/register', userController.registation);
+
 
 module.exports = router;
