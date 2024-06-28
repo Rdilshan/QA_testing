8aebe251f566820697195dd5e6b168d41f05f386
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-28 17:20:00 +0530

    user middlware

---
+++ Backend/middlware/userlogincheck.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const userverifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).send('Invalid Token');
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send('Invalid Token');
+    }
+    return next();
+};
+
+module.exports = userverifyToken;
