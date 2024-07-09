eb5681fa1abe0a240956c57a57cbbff044b5aba2
Randika Dilshan <rdilshan077788@gmail.com>
2024-07-09 17:03:53 +0530

    same changes do for env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './index.html' // Ensure this path is correct
-    }
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
   }
-});
+})
