b4cad931defc0434c9fe673d1eecf7b8d4780381
Randika Dilshan <rdilshan077788@gmail.com>
2024-07-17 17:35:18 +0530

    user route test

--- Backend/tests/unit/routes/adminRoutes.test.js
+++ Backend/tests/unit/routes/adminRoutes.test.js
@@ -1,7 +1,8 @@
 const request = require("supertest");
 const { app, server } = require("../../../index");
 
-describe("Admin Routes", () => {
+describe("Admin/user Routes", () => {
+
   beforeAll((done) => {
     server.on("listening", () => {
       console.log("Server started");
@@ -9,9 +10,11 @@ describe("Admin Routes", () => {
     });
   });
 
+
   afterAll((done) => {
     server.close(done); // Close server after all tests
   });
+
   test('POST /admin/create should return 200 and "User registered successfully"', async () => {
     const response = await request(app)
       .post("/admin/create")
@@ -27,4 +30,23 @@ describe("Admin Routes", () => {
 
     expect(response.status).toBe(200);
   });
+
+  test('POST /user/login should return 200 and "User login successfully"', async () => {
+    const response = await request(app)
+      .post("/user/login")
+      .send({ email: "test@gmail.com", password: "testpassword" });
+
+    expect(response.status).toBe(401);
+  });
+
+  test('GET /user/get should return 200 and "user deetails get"', async () => {
+    const response = await request(app)
+      .get("/user/get");
+
+    expect(response.status).toBe(403);
+  });
+
+
+
+
 });
