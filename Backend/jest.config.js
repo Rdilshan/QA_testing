f58388158c9d31e00382ddaa84f57f990a13cd1f
Randika Dilshan <rdilshan077788@gmail.com>
2024-08-04 07:32:20 +0530

    login test

---
+++ Backend/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    testTimeout: 10000, // Increase the timeout if necessary
+    testRegex: './selenium/.*\\.js$', // Look for test files in the selenium directory
+  };
+  
