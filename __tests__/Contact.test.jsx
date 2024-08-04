21a284211a78271c51545cd6285305f1e0f826bc
Randika Dilshan <rdilshan077788@gmail.com>
2024-08-04 06:03:05 +0530

    contact page unit test

--- __tests__/Contact.test.jsx
+++ __tests__/Contact.test.jsx
@@ -1,15 +1,15 @@
 // __tests__/Login.test.tsx
 import { render, screen } from '@testing-library/react'
-import LoginReg from '../src/pages/LoginReg'
+import ContactUS from '../src/pages/ContactUS'
 import { describe, expect, it } from 'vitest'
 import React from 'react'
 
 describe('LoginReg component', () => {
   it('renders the About Us section', () => {
-    render(<LoginReg />)
+    render(<ContactUS />)
 
 
-    const aboutUsSection = screen.getByText('Member Area');
+    const aboutUsSection = screen.getByText('Street Address');
     expect(aboutUsSection).toBeInTheDocument()
 
 
